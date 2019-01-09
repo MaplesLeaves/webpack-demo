c9aaad1a4a729c53e68b17f0b7fd3dc760e70a9e
mapleLeaves <15094052912@163.com>
2019-01-09 10:45:10 +0800

    git

--- src/print.js
+++ src/print.js
@@ -1,5 +1,8 @@
 export default function printMe() {
+let a = 15
+
   console.log('I get called from print.js!123123123');
   console.log('Updating print.js...456456')
+  console.error(`我的年龄是${a}`)
   // cosnole.log('I get called from print.js!');
 }
